548cdbb71f80ccd79e99c216f298458c15117955
Tebebe <tebebe@gmail.com>
2018-05-05 00:27:28 -0400

    updated view using viewbag

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -13,6 +13,19 @@ namespace ComicBookGallery.Controllers
 
         public ActionResult Detail()
         {
+
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final Issue! Witness the final hour</p>";
+            ViewBag.Artists = new string[]
+            {
+                 "Script: Dan Slott",
+                 "Pencils: Huberto Ramos",
+                 "Inks: Abebe Kebede",
+                 "Colors: Ayele Mulatu",
+                 "Letters: Almaz Mulatu"
+            };
+
             return View();
            
 
