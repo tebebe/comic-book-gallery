7684d75f4a281b64ec086ae24ffa2cfa28f4326d
tebebe.garedew <tebebe@gmail.com>
2018-05-04 20:41:24 -0400

    Added View Detail

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -13,14 +13,8 @@ namespace ComicBookGallery.Controllers
 
         public ActionResult Detail()
         {
-            
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the comic books Controller Class");
-
+            return View();
+           
 
         }
     }
