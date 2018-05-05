9bd9e4364d6efaca0c4455b0bdcf8e65dbcd9be4
tebebe.garedew <tebebe@gmail.com>
2018-05-04 20:22:18 -0400

    Changes made to controller detail action

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,20 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+
+
+
+        public ActionResult Detail()
+        {
+            
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+            }
+
+            return Content("Hello from the comic books Controller Class");
+
+
+        }
     }
 }
